a9facd50b6664e38f371f605e55fadc882959cbe
tony <tony6liu@gmail.com>
2023-07-31 14:55:19 +0800

    docker compose successfully work

--- my-app/src/utils/api.js
+++ my-app/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const serverApi = "http://node:5002/api";
+const serverApi = "http://localhost:5002/api";
 
 export const getRoomExists = async (roomId) => {
   const response = await axios.get(`${serverApi}/room-exists/${roomId}`);
